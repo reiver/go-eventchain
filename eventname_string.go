9e4a5e86e7f5c75f6b0d119159dd9a7bcb012729
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-11-25 14:56:26 -0800

    update docs

--- eventname_string.go
+++ eventname_string.go
@@ -3,7 +3,8 @@ package eventchain
 
 // String returns a string representation of the event name.
 //
-// String makes EventName fit the fmt.Stringer interface.
+// String makes EventName fit the `Stringer` interface, from the
+// Go built-in package "fmt".
 func (eventName internalEventName) String() string {
 	return eventName.s
 }
