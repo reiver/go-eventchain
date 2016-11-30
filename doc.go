6b20614cdf915b84fa986e937d85dd7a24cdda6b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-11-29 17:16:37 -0800

    added more docs

--- doc.go
+++ doc.go
@@ -1,7 +1,23 @@
 /*
 Package eventchain provides basic building blocks for doing Event Sourcing and Log-Driven Development.
 
-Example:
+Two concepts that this package has are: "data stores" and "event stores".
+
+At the high level, an 'event store' contains the events for a single thing, and a 'data store' contains
+one or more 'event stores'.
+
+So for example, you might have 'event stores' for "user events", "purchase events", "warehouse inventory events", etc.
+
+A little more low level....
+
+If this was being backed by something like a Postgres database, then in that specific case the 'data store'
+is the Postgres database, and an 'event store' is a specifc table in that Postgres database.
+
+Alternatively, if one is working log files , a 'data store' could be a directory on a server (containing log files),
+and an 'event store' could be a single log file.
+
+Here is an example of connecting to a Postgres backed 'data store', and then using that 'data store' to
+get access to an 'event store' within it.
 
 	import (
 		"github.com/reiver/go-eventchain"
